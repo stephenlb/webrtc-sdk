4d8a1fda450e2830b170644df65a93e579c9a985
Stephen Blum <blum.stephen@gmail.com>
2018-10-16 17:35:19 -0700

    fixed depricated url stun server list.

--- js/webrtc-v2.js
+++ js/webrtc-v2.js
@@ -54,31 +54,31 @@ const PHONE = window.PHONE = config => {
     // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     // STUN Server List Configuration (public STUN list)
     // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-    const rtcconfig = { iceServers : [{ "url" :
+    const rtcconfig = { iceServers : [{ "urls" :
         navigator.mozGetUserMedia    ? "stun:stun.services.mozilla.com" :
         navigator.webkitGetUserMedia ? "stun:stun.l.google.com:19302"   :
                                        "stun:23.21.150.121"
     }
-    ,   {url: "stun:stun.l.google.com:19302"}
-    ,   {url: "stun:stun1.l.google.com:19302"}
-    ,   {url: "stun:stun2.l.google.com:19302"}
-    ,   {url: "stun:stun3.l.google.com:19302"}
-    ,   {url: "stun:stun4.l.google.com:19302"}
-    ,   {url: "stun:23.21.150.121"}
-    ,   {url: "stun:stun01.sipphone.com"}
-    ,   {url: "stun:stun.ekiga.net"}
-    ,   {url: "stun:stun.fwdnet.net"}
-    ,   {url: "stun:stun.ideasip.com"}
-    ,   {url: "stun:stun.iptel.org"}
-    ,   {url: "stun:stun.rixtelecom.se"}
-    ,   {url: "stun:stun.schlund.de"}
-    ,   {url: "stun:stunserver.org"}
-    ,   {url: "stun:stun.softjoys.com"}
-    ,   {url: "stun:stun.voiparound.com"}
-    ,   {url: "stun:stun.voipbuster.com"}
-    ,   {url: "stun:stun.voipstunt.com"}
-    ,   {url: "stun:stun.voxgratia.org"}
-    ,   {url: "stun:stun.xten.com"}
+    ,   {urls: "stun:stun.l.google.com:19302"}
+    ,   {urls: "stun:stun1.l.google.com:19302"}
+    ,   {urls: "stun:stun2.l.google.com:19302"}
+    ,   {urls: "stun:stun3.l.google.com:19302"}
+    ,   {urls: "stun:stun4.l.google.com:19302"}
+    ,   {urls: "stun:23.21.150.121"}
+    ,   {urls: "stun:stun01.sipphone.com"}
+    ,   {urls: "stun:stun.ekiga.net"}
+    ,   {urls: "stun:stun.fwdnet.net"}
+    ,   {urls: "stun:stun.ideasip.com"}
+    ,   {urls: "stun:stun.iptel.org"}
+    ,   {urls: "stun:stun.rixtelecom.se"}
+    ,   {urls: "stun:stun.schlund.de"}
+    ,   {urls: "stun:stunserver.org"}
+    ,   {urls: "stun:stun.softjoys.com"}
+    ,   {urls: "stun:stun.voiparound.com"}
+    ,   {urls: "stun:stun.voipbuster.com"}
+    ,   {urls: "stun:stun.voipstunt.com"}
+    ,   {urls: "stun:stun.voxgratia.org"}
+    ,   {urls: "stun:stun.xten.com"}
     ] };
 
     // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
