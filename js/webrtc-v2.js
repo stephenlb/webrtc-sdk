bd71202dc5e0e38618cbf9e1d072ac7690f951b8
Stephen Blum <blum.stephen@gmail.com>
2020-04-30 11:56:33 -0700

    reduced STUN server by recommendation of https://github.com/stephenlb/webrtc-sdk/issues/76

--- js/webrtc-v2.js
+++ js/webrtc-v2.js
@@ -50,26 +50,26 @@ const PHONE = window.PHONE = config => {
         navigator.webkitGetUserMedia ? "stun:stun.l.google.com:19302"   :
                                        "stun:23.21.150.121"
     }
-    ,   {urls: "stun:stun.l.google.com:19302"}
+    //,   {urls: "stun:stun.l.google.com:19302"}
     ,   {urls: "stun:stun1.l.google.com:19302"}
     ,   {urls: "stun:stun2.l.google.com:19302"}
     ,   {urls: "stun:stun3.l.google.com:19302"}
-    ,   {urls: "stun:stun4.l.google.com:19302"}
-    ,   {urls: "stun:23.21.150.121"}
-    ,   {urls: "stun:stun01.sipphone.com"}
-    ,   {urls: "stun:stun.ekiga.net"}
-    ,   {urls: "stun:stun.fwdnet.net"}
-    ,   {urls: "stun:stun.ideasip.com"}
-    ,   {urls: "stun:stun.iptel.org"}
-    ,   {urls: "stun:stun.rixtelecom.se"}
-    ,   {urls: "stun:stun.schlund.de"}
-    ,   {urls: "stun:stunserver.org"}
-    ,   {urls: "stun:stun.softjoys.com"}
-    ,   {urls: "stun:stun.voiparound.com"}
-    ,   {urls: "stun:stun.voipbuster.com"}
-    ,   {urls: "stun:stun.voipstunt.com"}
-    ,   {urls: "stun:stun.voxgratia.org"}
-    ,   {urls: "stun:stun.xten.com"}
+    //,   {urls: "stun:stun4.l.google.com:19302"}
+    //,   {urls: "stun:23.21.150.121"}
+    //,   {urls: "stun:stun01.sipphone.com"}
+    //,   {urls: "stun:stun.ekiga.net"}
+    //,   {urls: "stun:stun.fwdnet.net"}
+    //,   {urls: "stun:stun.ideasip.com"}
+    //,   {urls: "stun:stun.iptel.org"}
+    //,   {urls: "stun:stun.rixtelecom.se"}
+    //,   {urls: "stun:stun.schlund.de"}
+    //,   {urls: "stun:stunserver.org"}
+    //,   {urls: "stun:stun.softjoys.com"}
+    //,   {urls: "stun:stun.voiparound.com"}
+    //,   {urls: "stun:stun.voipbuster.com"}
+    //,   {urls: "stun:stun.voipstunt.com"}
+    //,   {urls: "stun:stun.voxgratia.org"}
+    //,   {urls: "stun:stun.xten.com"}
     ] };
 
     // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
